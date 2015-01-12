3cb538ada22998e740a11c85ef8a02fa85a4d883
keailufei <137236151@qq.com>
2015-01-12 11:40:40 +0800

    增加把table导出为excel的函数
    增加把table导出为excel的函数

--- COCOUtil/COCOUtilCohesion.js
+++ COCOUtil/COCOUtilCohesion.js
@@ -70,6 +70,69 @@ define([], function() {
 	cohesion.getErrorMessage = function(err) {
 		return err.message;
 	};
+	
+	    /**
+	 * @description 将数据导出Excel
+	 * @param {String} tableid  table的id
+	 * @since 2015年1月12日 11:39:09
+	 * @return 电子表格
+	 */
+	    cohesion.exportExcel = function(tableid){
+	    	//来自《超实用的javaScript代码段》最后一章
+	        if (!!+[1,]){//判断是否为IE
+	            alert("不是IE浏览器，不支持此方法！");
+	            return;
+	        }
+	        var curTbl = document.getElementById(tableid);
+	                /*
+	                * 如果IE浏览器报错：SCRIPT429: Automation 服务器不能创建对象
+	                * 打开Internet Explorer “工具”菜单栏中的“选项”一栏，单击“安全”栏中的“自定义级别”选项卡，
+	                * 将第三项“对没有标记为安全的activex控件进行初始化和脚本运行”设置成“启用”即可
+	                * */
+	
+	                oXL = new ActiveXObject("Excel.Application"),
+	
+	                oWB = oXL.Workbooks.Add(),//创建AX对象excel
+	
+	                xlsheet = oWB.Worksheets(1),//获取workbook对象
+	
+	                sel = document.body.createTextRange();//激活当前sheet
+	
+	            sel.moveToElementText(curTbl);//把表格中的内容移到TextRange中
+	
+	            sel.select();//全选TextRange中内容
+	
+	            sel.execCommand("Copy");//复制TextRange中内容
+	
+	            xlsheet.Paste();//粘贴到活动的EXCEL中
+	
+	            oXL.Visible = true;//设置excel可见属性
+	
+	            try{
+	                var fname = oXL.Application.GetSaveAsFilename("save.xls", "Excel Spreadsheets (*.xls), *.xls");
+	            }catch(error){
+	                print("Nested catch caught " + error);
+	            }finally{
+	                if(fname){
+	                    oWB.SaveAs(fname);
+	                    oWB.Close(savechanges=false);
+	
+	                    oXL.Quit();
+	
+	                    oXL=null;
+	                }else{
+	                    alert("导出失败");
+	                }
+	
+	
+	
+	                _W.setTimeout(function(){//结束excel进程，退出完成
+	                    CollectGarbage();
+	                },1);
+	
+	            }
+	    }
+    
 
 	//isNaN 不是有效数字字符串，返回true
 	return cohesion;
