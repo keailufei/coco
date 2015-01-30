d4a57839a0ea0f2872133e24ef9a9e86a537dfca
keailufei <137236151@qq.com>
2015-01-30 15:36:40 +0800

    增加jsdoc注释，精简代码结构
    增加jsdoc注释，精简代码结构

--- UrlUtil.js
+++ UrlUtil.js
@@ -1,18 +1,35 @@
-define(function() { /**功能：返回当前页面的url
-	 * 入参：无
-	 * 作者：陈长安
-	 * 创建时间：2014-07-04
-	 * 修改时间：*/
-	var getURL = function() {
-		return document.URL;//大写哦！
-	};
+/**
+ * @fileOverview 文件描述：URL工具文件
+ * @author chencha
+ * @version 2.0
+ * @since 2015年1月30日 14:07:09
+ * @history
+ **************************************************************
+ * 修改历史
+ * 序号			日期		修改人		修改原因
+ * 1
+ * 2
+ **************************************************************
+ */
+define({
+	/**
+	 * 返回当前页面的url
+	 * @author chencha
+	 * @since 2014-07-04
+	 *
+	 */
+	getURL: function() {
+		return document.URL; //大写哦！
+	},
 
-	/**功能：返回url参数数组，分别有name和value个属性
-	 * 入参：URL字符串
-	 * 作者：陈长安
-	 * 创建时间：2014-07-04
-	 * 修改时间：*/
-	var getURLParams = function(url) {
+
+	/** 返回url参数数组，分别有name和value个属性
+	 * @param {String} url - url字符串
+	 * @author chencha
+	 * @since 2014-07-04
+	 *
+	 */
+	getURLParams: function(url) {
 		var params = [];
 		if (url.indexOf('?') > -1) {
 			var a1 = url.split('?');
@@ -22,21 +39,22 @@ define(function() { /**功能：返回当前页面的url
 				a3 = a2[i].split('=');
 				if (a3.length == 2) {
 					params.push({
-						'name' : a3[0],
-						'value' : a3[1]
+						'name': a3[0],
+						'value': a3[1]
 					})
 				}
 			}
 		}
 		return params;
-	};
+	},
 
-	/**功能：返回URL对象，属性是URL的参数们
-	 * 入参：URL字符串
-	 * 作者：陈长安
-	 * 创建时间：2014-07-04
-	 * 修改时间：*/
-	var getURLObject = function(url) {
+
+	/** 返回URL对象，属性是URL的参数们
+	 * @param {String} url URL字符串
+	 * @author chencha
+	 * @since 2014-07-04
+	 */
+	getURLObject: function(url) {
 		var URLUtil = this;
 		var res = {};
 		var urlParams = URLUtil.getURLParams(url);
@@ -47,24 +65,19 @@ define(function() { /**功能：返回当前页面的url
 			res[name] = value;
 		};
 		return res;
-	};
+	},
 
-	/**功能：返回URL中的指定参数
-	 * 入参：URL字符串，参数名
-	 * 作者：陈长安
-	 * 创建时间：2014-07-04
-	 * 修改时间：*/
-	var getUrlParamByName = function(url, paramName) {
+
+	/** 返回URL中的指定参数
+	 * @param {String} url URL字符串
+	 * @param {String} paramName 参数名
+	 * @author chencha
+	 * @since 2014-07-04
+	 */
+	getUrlParamByName: function(url, paramName) {
 		var URLUtil = this;
 		var urlObj = URLUtil.getURLObject(url);
 		var res = urlObj[paramName];
 		return res;
-	};
-	
-    return { 
-       getUrlParamByName:getUrlParamByName,
-       getURLObject:getURLObject,
-       getURLParams:getURLParams,
-       getURL:getURL
-    }; 
-}); 
+	},
+});
