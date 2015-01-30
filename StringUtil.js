9c987fdb4ae279be84bfcc4a1633d0f5b2b9c264
keailufei <137236151@qq.com>
2015-01-30 16:33:57 +0800

    jsdoc注释，简化amd写法

--- StringUtil.js
+++ StringUtil.js
@@ -1,31 +1,59 @@
-define(function() {
-	/**功能：返回去掉左右空格的字符串
-	 *入参：目标字符串
-	 * 作者：陈长安
-	 * 创建时间：2014-07-07
-	 * 修改时间：*/
-	function trim(str) {
+define({
+
+	/** 返回去掉左右空格的字符串
+	 * @param {String} str 目标字符串
+	 * @author chencha
+	 * @since 2014-07-07
+	 */
+	trimLAndR: function(str) {
 		var regExp = /(^\s*)|(\s*$)/g;
 		//验证左右空格的正则表达式
 		str = str.replace(regExp, "");
 		//去掉字符串的左右空格
 		return str;
-	}
-	
-	　/**删除左边的空格*/
-　　 function ltrim(str){
-　　     return str.replace(/(^\s*)/g,"");
-　　 }
-	
-	/**删除右边的空格*/
-　　 function rtrim(str){ 
-　　     return str.replace(/(\s*$)/g,"");
-　　 }
-	
+	},
 
-	return {
-		trim : trim,
-		ltrim:ltrim,
-		rtrim:rtrim
-	};
+	/** 去掉字符串全部空格
+	 * @param {String} str 目标字符串
+	 * @author chencha
+	 * @since 2014-07-07
+	 */
+	trimAll: function(str) {
+		var regExp = /\s/g;
+		str = str.replace(regExp, "");
+		return str;
+	},
+
+	/** 删除左边的空格
+	 * @param {String} str 目标字符串
+	 * @author chencha
+	 */
+	ltrim: function(str) {
+		return str.replace(/(^\s*)/g, "");
+	},
+
+
+	/** 删除右边的空格
+	 * @param {String} str 目标字符串
+	 * @author chencha
+	 */
+	rtrim: function(str) {
+
+		return str.replace(/(\s*$)/g, "");
+	},
+
+
+	/** 小写字母——>大写字母
+	 * @param {String} str 目标字符串
+	 * @param {String} type 是否采取本地转换格式，不传也能用
+	 * @author chencha
+	 */
+	getUpCase: function(str, type) {
+
+		type = type || "locale"; //是否采取本地转换格式
+
+		return type === "locale" && str.toLocaleUpperCase() || str.toUpperCase(); //返回转换后的值
+	}
+
+
 });
