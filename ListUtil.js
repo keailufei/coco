aabc837cc780923431cfc2390be3ca62b1d37776
keailufei <137236151@qq.com>
2015-02-02 14:15:50 +0800

    简化代码，增加jsdoc3的注释

--- ListUtil.js
+++ ListUtil.js
@@ -1,11 +1,12 @@
-define(function() {
-	
-	/**功能：返回列表类对象是否有效（对象有效、有length属性、长度大于-1）
-	 * 入参：列表类对象
-	 * 作者：陈长安
-	 * 创建时间：2014-07-011
-	 * 修改时间：*/
-	function isListValid(list) {
+define({
+
+
+	/** 返回列表类对象是否有效（对象有效、有length属性、长度大于-1）
+	 * @author chencha
+	 * @since 2014-07-011
+	 * @param {Object} list 列表类对象
+	 */
+	isListValid: function(list) {
 		if (!!list) {
 			if (list.length == 0) {
 				return true;
@@ -20,14 +21,17 @@ define(function() {
 		} else {
 			return false;
 		}
-	}
+	},
 
-	/**功能：返回目标值是否在列表中存在（参数无效时返回undefined）
-	 * 入参：（目标值，列表，目标值与列表中vo比较哪个属性（可选））
-	 * 作者：陈长安
-	 * 创建时间：2014-07-011
-	 * 修改时间：*/
-	function isInList(target, list, propertyName) {
+
+	/** 返回目标值是否在列表中存在（参数无效时返回undefined）
+	 * @author chencha
+	 * @since 2014-07-011
+	 * @param {Object} target 目标值
+	 * @param {Object} list 列表
+	 * @param {Object} propertyName 目标值与列表中vo比较哪个属性（可选）
+	 */
+	isInList: function(target, list, propertyName) {
 		var myListUtil = this;
 		if (myListUtil.isListValid(list)) {
 			var length = list.length;
@@ -40,7 +44,7 @@ define(function() {
 					var voValue = vo;
 					var targetValue = target;
 				}
-				if (voValue == targetValue && voValue != undefined) {//存在
+				if (voValue == targetValue && voValue != undefined) { //存在
 					return true;
 				}
 			}
@@ -50,14 +54,17 @@ define(function() {
 			alert("bad inputs");
 			return undefined;
 		}
-	}
+	},
 
-	/**功能：过滤出：在列表A中、不在列表B中的对象们
-	 * 入参：（列表A，列表B，比较的值对应列表vo中的属性名（可选））
-	 * 作者：陈长安
-	 * 创建时间：2014-07-011
-	 * 修改时间：*/
-	function filterInANotInB(listA, listB, propertyName) {
+
+	/** 过滤出：在列表A中、不在列表B中的对象们
+	 * @author chencha
+	 * @since 2014-07-011
+	 * @param {Object} listA
+	 * @param {Object} listB
+	 * @param {Object} propertyName 比较的值对应列表vo中的属性名（可选）
+	 */
+	filterInANotInB: function(listA, listB, propertyName) {
 		var myListUtil = this;
 		var res = [];
 		if (myListUtil.isListValid(listA) && myListUtil.isListValid(listB)) {
@@ -66,22 +73,25 @@ define(function() {
 			for (var idxA = 0; idxA < lengthA; idxA++) {
 				var vo = listA[idxA];
 				var isInB = myListUtil.isInList(vo, listB, propertyName);
-				if (isInB == false) {//没在列表B中
+				if (isInB == false) { //没在列表B中
 					res.push(vo);
 				}
-			}//end of 遍历列表A
+			} //end of 遍历列表A
 		} else {
 			alert("bad inputs");
 		}
 		return res;
-	}
-	
-	/**功能：过滤出2个列表中同时存在的
-	 * 入参：（列表A，列表B，比较的值对应列表vo中的属性名（可选））
-	 * 作者：陈长安
-	 * 创建时间：2014-11-05
-	 * 修改时间：*/
-	function filterBothExist(listA, listB, propertyName) {
+	},
+
+
+	/** 过滤出2个列表中同时存在的
+	 * @author chencha
+	 * @since 2014-07-011
+	 * @param {Object} listA 列表A
+	 * @param {Object} listB 列表B
+	 * @param {Object} propertyName 比较的值对应列表vo中的属性名（可选）
+	 */
+	filterBothExist: function(listA, listB, propertyName) {
 		var myListUtil = this;
 		var res = [];
 		if (myListUtil.isListValid(listA) && myListUtil.isListValid(listB)) {
@@ -90,51 +100,64 @@ define(function() {
 			for (var idxA = 0; idxA < lengthA; idxA++) {
 				var vo = listA[idxA];
 				var isInB = myListUtil.isInList(vo, listB, propertyName);
-				if (isInB == true) {//在列表B中
+				if (isInB == true) { //在列表B中
 					res.push(vo);
 				}
-			}//end of 遍历列表A
+			} //end of 遍历列表A
 		} else {
 			alert("bad inputs");
 		}
 		return res;
-	}
-	
-	//从list中取出第一个属性值对应的对象
-	//add by chencha in 2014年11月11日 14:32:32
-	function getObjInList(list,propertyName,propertyValue){
+	},
+
+
+	/** 从list中取出第一个属性值对应的对象
+	 * @author chencha
+	 * @since 2014年11月11日 14:32:32
+	 * @param {Object} list
+	 * @param {Object} propertyName
+	 * @param {Object} propertyValue
+	 */
+	getObjInList: function(list, propertyName, propertyValue) {
 		var myListUtil = this;
-		if(myListUtil.isListValid(list)){
-			var length=list.length;
-			for(var i=0;i<length;i++ ){
-				var obj=list[i];
-				if(obj[propertyName]==propertyValue){
+		if (myListUtil.isListValid(list)) {
+			var length = list.length;
+			for (var i = 0; i < length; i++) {
+				var obj = list[i];
+				if (obj[propertyName] == propertyValue) {
 					return obj;
 				}
-				
+
 			}
 		}
 		return null;
-	}
-	
-	// 从list中取出第一个属性值对应的对象的目标属性值
-	//add by chencha in 2014年11月11日 14:32:32
-	function getTargetValue(list,propertyName,propertyValue,targetProperty){
-		var obj=this.getObjInList(list,propertyName,propertyValue);
-		var res=undefined;
-		if(!!obj){
-			res=obj[targetProperty];
-		}
-		return res;	
-	}
-	
+	},
 
-	return {
-		isListValid : isListValid,
-		isInList : isInList,
-		filterInANotInB : filterInANotInB,
-		filterBothExist:filterBothExist,
-		getObjInList:getObjInList,
-		getTargetValue:getTargetValue
-	};
+
+	/** 从list中取出第一个属性值对应的对象的目标属性值
+	 *@author chencha
+	 * @since 2014年11月11日 14:32:32
+	 * @param {Object} list
+	 * @param {Object} propertyName
+	 * @param {Object} propertyValue
+	 * @param {Object} targetProperty
+	 */
+	getTargetValue: function(list, propertyName, propertyValue, targetProperty) {
+		var obj = this.getObjInList(list, propertyName, propertyValue);
+		var res = undefined;
+		if (!!obj) {
+			res = obj[targetProperty];
+		}
+		return res;
+	}
+
+
+	// return {
+	// isListValid: isListValid,
+	// isInList: isInList,
+	// filterInANotInB: filterInANotInB,
+	// filterBothExist: filterBothExist,
+	// getObjInList: getObjInList,
+	// getTargetValue: getTargetValue
+	// };
 });
